1270b780fdb26ac7aa57b09dbcfec8d905413b6b
Olstra <44567931+Olstra@users.noreply.github.com>
2020-03-08 20:52:00 +0100

    created automatic script

--- BASH stuff/latex.sh
+++ BASH stuff/latex.sh
@@ -1,7 +1,15 @@
-# LateX
+#!/bin/bash
+
+# LateX compiler script
+
+echo "filename (without .tex): "
+read name
 
 # compile tex to dvi, convert dvi to pdf
-latex latex_filet.tex; dvipdf latex_filet.dvi 
+latex $name.tex 
+
+# convert dvi to pdf
+dvipdf $name.dvi 
 
 # view pdf file
-xdvi latex_filet.dvi
+xdvi $name.dvi
